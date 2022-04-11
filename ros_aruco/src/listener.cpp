4cc85c95666e1cf1b8c064d019bc9bdc5f9ed08b
Zhanat Makhataeva <zhanat.mahataeva@nu.edu.kz>
2022-04-11 15:16:20 +0600

    Add files via upload

---
+++ ros_aruco/src/listener.cpp
@@ -0,0 +1,27 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+/**
+ * This tutorial demonstrates simple receipt of messages over the ROS system.
+ */
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+  
+  ros::init(argc, argv, "listener");
+
+  
+  ros::NodeHandle n_h_;
+
+  
+  ros::Subscriber sub = n_h_.subscribe("tcptopic", 1000, chatterCallback);
+
+  
+  ros::spin();
+
+  return 0;
+}
